c45b37a68570f07f5d2ed6132f473ccac69032bb
emon <tj.emon33@gmail.com>
2025-06-23 03:31:50 +0600

    Fix filename casing of SignUp.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import UserAuth from "./pages/authPage/UserAuth";
 import HomePage from "./pages/homePage/HomePage";
 import SignIn from "./pages/authPage/SignIn";
-import SignUp from "./pages/authPage/Signup";
+import SignUp from "./pages/authPage/SignUp";
 import ProtectedRoute from "./ui/ProtectedRoute";
 
 export const router = createBrowserRouter([
