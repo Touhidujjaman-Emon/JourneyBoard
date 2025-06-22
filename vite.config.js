4dc3aba00986d50a3dff40df6decaf22121f0595
emon <tj.emon33@gmail.com>
2025-06-23 03:18:38 +0600

    fixed eslint

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), tailwindcss()],
+  plugins: [
+    react(),
+    eslint({
+      emitWarning: true,
+      emitError: false,
+      failOnWarning: false,
+      failOnError: false,
+    }),
+    tailwindcss(),
+  ],
 });
